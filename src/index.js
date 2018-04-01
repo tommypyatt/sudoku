2ab48191e00974e7af0c1b1ad8d3f5f264af3473
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2018-04-01 19:11:16 +0100

    Make example puzzle a take a little more to complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Numpad } from './components/Numpad';
 class App extends React.Component {
     constructor (props) {
         var i = 0;
-        var values = [null,null,5,2,6,9,7,8,1,6,8,2,5,7,1,4,9,3,1,9,7,8,3,4,5,6,2,8,2,6,1,9,5,3,4,7,3,7,4,6,8,2,9,1,5,9,5,1,7,4,3,6,2,8,5,1,9,3,2,6,8,7,4,2,4,8,9,5,7,1,3,6,7,6,3,4,1,8,2,5,9]
+        var values = [null,null,5,2,6,9,7,8,1,6,null,2,5,7,1,4,9,3,1,9,7,8,3,null,5,6,2,8,2,6,1,9,5,3,4,7,3,null,4,6,8,2,9,1,5,9,5,1,7,4,3,6,2,null,5,1,9,3,2,null,8,7,4,2,4,8,null,5,7,1,3,6,7,6,3,4,1,8,null,5,9]
         var cells = [];
 
         for (;i < 81;i++) {
