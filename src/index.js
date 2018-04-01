11eb3d3040647ba00faa5a9e7bc1b9ecb46c03bd
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2018-04-01 22:49:25 +0100

    Optimise completion detection logic
    Add some comments to make things clearer

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ import { Numpad } from './components/Numpad';
 class App extends React.Component {
     constructor (props) {
         var i = 0;
-        var values = '9...718..47.9.....6.2..37...8.45.3....7....9639..2.5.1..1....4....58.6...23.....5'
+        var values = '9...718..47.9.....6.2..37...8.45.3....7....9639..2.5.1..1....4....58.6...23.....5';
         var cells = [];
 
+        super(props);
+
         for (;i < 81;i++) {
+            // Create initial cells. Pre-calculate some useful things.
             let row = Math.floor(i / 9);
             let column = i % 9;
-            let box = (Math.floor(row / 3) * 3) + Math.floor(column / 3);
+            let box = (Math.floor(row / 3) * 3) + Math.floor(column / 3); // Box is segment when cells
+                                                                          // are mapped to 3 x 3 grid
             let value = values.slice(i, i + 1);
             let isDisabled = value !== '.';
 
@@ -27,92 +31,82 @@ class App extends React.Component {
             });
         }
 
-        super(props);
         this.state = {
             cells: cells,
-            difficulty: 'Easy',
             selectedCell: null,
             complete: false
         }
 
+        // Bind `this` keyword on certain functions
         this.selectCell = this.selectCell.bind(this);
         this.press = this.press.bind(this);
     }
 
     selectCell (cell) {
+        // Cell has been clicked
         this.setState({
             selectedCell: cell
         });
     }
 
     press (key) {
+        // Number has been clicked
         var selectedCell = this.state.selectedCell;
-        var index = _.findIndex(this.state.cells, function (cell) {
+        var index = _.findIndex(this.state.cells, (cell) => {
             return cell.index === selectedCell
         });
 
         if (this.state.cells[selectedCell].disabled) {
+            // If cell is in initial state, do nothing when clicked
             return;
         }
 
         if (index !== -1) {
+            // If a cell is selected, change its value
             this.state.cells[index].value = key;
 
             this.setState({
                 cells: this.state.cells
             });
-
-            this.updateComplete();
         }
     }
 
-    updateComplete () {
-        var complete = true;
+    check (cells, group) {
+        // Function to check groups contain all of numbers 1-9.
+        // 'Groups' can be 'row', 'column' or 'box'. Returns boolean.
+        var sets = [0,1,2,3,4,5,6,7,8];
 
-        _.forEach(this.state.cells, function (cell) {
-            if (!this.isCellOk(cell)) {
-                complete = false;
-            }
-        }.bind(this));
-
-        if (complete === true) {
-            this.setState({
-                complete: true
+        return _.every(sets, (set) => {
+            // Collect cell objects
+            var groupCells = _.filter(cells, (cell) => {
+                return cell[group] === set;
             });
-        }
+
+            // Flatten to values
+            var values = _.flatMap(groupCells, (cell) => {
+                return cell.value;
+            });
+
+            // Sort alphanumerically
+            values.sort();
+
+            // Are all numbers in group?
+            return _.isEqual(values, [1,2,3,4,5,6,7,8,9]);
+        });
     }
 
-    isCellOk (cell) {
-        var row = _.filter(this.state.cells, function (c) {
-            return c.row === cell.row;
-        });
-        var col = _.filter(this.state.cells, function (c) {
-            return c.column === cell.column;
-        });
-        var box = _.filter(this.state.cells, function (c) {
-            return c.box === cell.box;
-        });
-
-        var flatRow = _.flatMap(row, function (item) {
-            return item.value
-        }).sort();
-        var flatCol = _.flatMap(col, function (item) {
-            return item.value
-        }).sort();
-        var flatBox = _.flatMap(col, function (item) {
-            return item.value
-        }).sort();
-
-        return _.isEqual(flatRow, [1,2,3,4,5,6,7,8,9])
-            && _.isEqual(flatCol, [1,2,3,4,5,6,7,8,9])
-            && _.isEqual(flatBox, [1,2,3,4,5,6,7,8,9]);
+    isComplete () {
+        // Return true if numbers 1-9 exist for all groups; 'row', 'column', and 'box'
+        return this.check(this.state.cells, 'row')
+            && this.check(this.state.cells, 'column')
+            && this.check(this.state.cells, 'box')
     }
 
     render () {
         return <div>
             <h1>Sudoku</h1>
             <div className='grid-wrap'>
-                <div className='win-overlay' style={{'display': this.state.complete ? 'block' : 'none'}}><h2>You win</h2></div>
+                <div className='win-overlay' style={{'display': this.isComplete() ? 'block' : 'none'}}><h2>You win</h2></div>
                 <Grid cells={this.state.cells} selectCell={this.selectCell} selectedCell={this.state.selectedCell} />
             </div>
             <Numpad press={this.press} />
