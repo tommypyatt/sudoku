c3f979cb3b4adbe8ce17145ab7d7351b045c27e6
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2018-04-02 14:36:50 +0100

    Update for loop to ES6 type for readability
    Also make some modifications to index.js to remove some unnecessay logic

--- src/index.js
+++ src/index.js
@@ -6,30 +6,31 @@ import { Numpad } from './components/Numpad';
 
 class App extends React.Component {
     constructor (props) {
-        var i = 0;
+        var index = 0;
         var values = '9...718..47.9.....6.2..37...8.45.3....7....9639..2.5.1..1....4....58.6...23.....5';
         var cells = [];
 
-        super(props);
-
-        for (;i < 81;i++) {
+        for (var value of values) {
             // Create initial cells. Pre-calculate some useful things.
-            let row = Math.floor(i / 9);
-            let column = i % 9;
+            let row = Math.floor(index / 9);
+            let column = index % 9;
             let box = (Math.floor(row / 3) * 3) + Math.floor(column / 3); // Box is segment when cells
                                                                           // are mapped to 3 x 3 grid
-            let value = values.slice(i, i + 1);
             let isDisabled = value !== '.';
 
             cells.push({
-                index: i,
+                index: index,
                 value: isDisabled ? parseInt(value, 10) : '',
                 disabled: isDisabled,
                 row: row,
                 column: column,
                 box: box
             });
+
+            ++index;
         }
+
+        super(props);
 
         this.state = {
             cells: cells,
@@ -52,18 +53,15 @@ class App extends React.Component {
     press (key) {
         // Number has been clicked
         var selectedCell = this.state.selectedCell;
-        var index = _.findIndex(this.state.cells, (cell) => {
-            return cell.index === selectedCell
-        });
 
         if (this.state.cells[selectedCell].disabled) {
             // If cell is in initial state, do nothing when clicked
             return;
         }
 
-        if (index !== -1) {
+        if (selectedCell) {
             // If a cell is selected, change its value
-            this.state.cells[index].value = key;
+            this.state.cells[selectedCell].value = key;
 
             this.setState({
                 cells: this.state.cells
@@ -97,14 +95,16 @@ class App extends React.Component {
 
     isComplete () {
         // Return true if numbers 1-9 exist for all groups; 'row', 'column', and 'box'
-        return this.check(this.state.cells, 'row')
-            && this.check(this.state.cells, 'column')
-            && this.check(this.state.cells, 'box')
+        var cells = this.state.cells;
+
+        return this.check(cells, 'row')
+            && this.check(cells, 'column')
+            && this.check(cells, 'box')
     }
 
     render () {
         return <div>
-            <h1>Sudoku</h1>
+            <h1 className='page-title'>Sudoku</h1>
             <div className='grid-wrap'>
                 <div className='win-overlay' style={{'display': this.isComplete() ? 'block' : 'none'}}><h2>You win</h2></div>
                 <Grid cells={this.state.cells} selectCell={this.selectCell} selectedCell={this.state.selectedCell} />
